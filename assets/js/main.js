f6569b1a724e49095a698d508076e4a0e5963cab
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-03-23 14:30:36 +0100

    Fine esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// Chiedi all’utente il suo nome
+const first_name = prompt('Inserire nome');
+
+console.log(first_name);
+
+// Chiedi il suo cognome
+const last_name = prompt('Inserire cognome');
+
+console.log(last_name);
+
+// Chiedi il suo colore preferito
+const color = prompt('Inserisci il tuo colore preferito');
+
+console.log(color);
+
+// Scrivi sulla pagina il risultato 
+const result = `${first_name}${last_name}${color}${'22'}`;
+
+document.getElementById('result').innerHTML = result;
+
+console.log(`${first_name}${last_name}${color}${'22'}`);
+
